ee3a9ab7d46d3122a8541dbbec818bcc1c1e82b9
Hernandez Vincent <vincent.hernandez1985@gmail.com>
2018-07-06 20:45:13 +0900

    Update

--- DNN.py
+++ DNN.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 import seaborn as sns
 from sklearn import metrics
 from tensorflow.examples.tutorials.mnist import input_data
-import random
 
 n_sample_train = 10000
 n_sample_test = 1000
@@ -21,7 +20,7 @@ def plot_MNIST(x, one_hot):
 
     row = 4
     column = 4
-    p = random.sample(range(1, 100), row * column)
+    p = [0, 12, 54, 45, 17, 12, 45, 89, 154, 111, 156, 8, 13, 14, 15, 16]
 
     plt.figure()
 
@@ -46,6 +45,26 @@ def confusion_matrix(cm, accuracy):
     all_sample_title = 'Accuracy Score: {0}'.format(accuracy)
     plt.title(all_sample_title, size=15)
 
+def dense(input, name, in_size, out_size, activation="relu"):
+
+    with tf.variable_scope(name, reuse=False):
+        w = tf.get_variable("w", shape=[in_size, out_size],
+                            initializer=tf.random_normal_initializer(mean=0, stddev=0.1))
+        b = tf.get_variable("b", shape=[out_size], initializer=tf.constant_initializer(0.0))
+
+        l = tf.add(tf.matmul(input, w), b)
+
+        if activation == "relu":
+            l = tf.nn.relu(l)
+        elif activation == "sigmoid":
+            l = tf.nn.sigmoid(l)
+        elif activation == "tanh":
+            l = tf.nn.tanh(l)
+        else:
+            l = l
+    print(l)
+    return l
+
 train_x, one_hots_train, test_x, one_hots_test = get_MNIST_data()
 number_test = [one_hots_test[i, :].argmax() for i in range(0, one_hots_test.shape[0])]
 
@@ -58,12 +77,20 @@ height = train_x.shape[1]               # All the pixels are represented as a ve
 hyperparameters = {}
 
 # Session and context manager
+tf.reset_default_graph()
+sess = tf.Session()
 
+with tf.variable_scope(tf.get_variable_scope()):
 
     # Placeholders
+    x = tf.placeholder(tf.float32, [None, height], name='X')
+    y = tf.placeholder(tf.float32, [None, n_label], name='Y')
 
-
+    print(x)
     # Neural network
+    l1 = dense(input=x, name="layer_1", in_size=height, out_size=10, activation="relu")
+    l2 = dense(input=l1, name="layer_2", in_size=10, out_size=10, activation="relu")
+    y_ = dense(input=l2, name="output_layer", in_size=10, out_size=n_label, activation="None")
 
     # Softmax layer
 
